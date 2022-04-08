d7475ea16637c9e7b6d13cfcd85de1d1a7a8673c
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-04-08 19:53:42 +0600

    Scaffold & appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text("hello net ninja app"),
-  ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("my app"),
+            centerTitle: true,
+          ),
+          body: const Center(
+            child: Text("hello ninja app working"),
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              // Add your onPressed code here!
+            },
+            label: const Text('Approve'),
+            icon: const Icon(Icons.thumb_up),
+            backgroundColor: Colors.pink,
+          ))));
 }
