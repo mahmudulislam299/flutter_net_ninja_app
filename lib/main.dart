22dd88c952c6fe6470bf322387e74f0cf8b695af
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-04-11 17:31:26 +0600

    Lists of Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,171 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: NinjaCard()));
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
 }
 
-class NinjaCard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
-  int taskDone = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'I will win',
+    'I have talent',
+    'I will shine',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[100],
-      appBar: AppBar(
-        title: const Text('PraniSheba ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        elevation: 0, //removing shadow in app bar
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            taskDone += 1;
-          });
-        },
-        child: const Icon(Icons.toys),
-        backgroundColor: Colors.green[800],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/cow2.jpg'),
-                radius: 80,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.grey[500],
-                letterSpacing: 2,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              //for spacing
-              height: 10,
-            ),
-            Text(
-              'Mahmudul Islam',
-              style: TextStyle(
-                color: Colors.grey[900],
-                letterSpacing: 2,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              //for spacing
-              height: 30,
-            ),
-            Text(
-              'Designation',
-              style: TextStyle(
-                color: Colors.grey[500],
-                letterSpacing: 2,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              //for spacing
-              height: 10,
-            ),
-            Text(
-              'IoT Engineer',
-              style: TextStyle(
-                color: Colors.grey[900],
-                letterSpacing: 2,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              //for spacing
-              height: 30,
-            ),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[900],
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'mahmudulislam299@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[900],
-                    fontSize: 18,
-                    letterSpacing: 1,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              //for spacing
-              height: 30,
-            ),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.mobile_friendly_sharp,
-                  color: Colors.grey[900],
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  '01811177722',
-                  style: TextStyle(
-                    color: Colors.grey[900],
-                    fontSize: 18,
-                    letterSpacing: 1,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              //for spacing
-              height: 30,
-            ),
-            Text(
-              'Task Done',
-              style: TextStyle(
-                color: Colors.grey[500],
-                letterSpacing: 2,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              //for spacing
-              height: 10,
-            ),
-            Text(
-              '$taskDone',
-              style: TextStyle(
-                color: Colors.grey[900],
-                letterSpacing: 2,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
+        backgroundColor: Colors.green[100],
+        appBar: AppBar(
+          title: const Text('Awesome quote'),
+          centerTitle: true,
+          backgroundColor: Colors.green[800],
         ),
-      ),
-    );
+        body: Column(
+          children: quotes.map((quote) => Text(quote)).toList(),
+        ));
   }
 }
