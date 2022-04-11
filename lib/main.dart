191baf651e81b3b1bc2b39421086c949e55ef838
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-04-11 19:10:37 +0600

    cards

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,41 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'kazi', text: 'chiro unnoto momo shir')
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Center(
+              child: Text(
+                quote.text,
+                style: TextStyle(
+                  color: Colors.green[600],
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Center(
+              child: Text(
+                quote.author,
+                style: TextStyle(
+                  color: Colors.green[800],
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,9 +64,7 @@ class _QuoteListState extends State<QuoteList> {
           backgroundColor: Colors.green[800],
         ),
         body: Column(
-          children: quotes
-              .map((quote) => Text('${quote.text} - ${quote.author}'))
-              .toList(),
+          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
         ));
   }
 }
