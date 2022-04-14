9d3c10dc9df36d1f57d7d50de4dae7c4c8d1ebb3
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-04-14 11:38:17 +0600

    starting world time app

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,8 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuoteList(),
+    home: Home(),
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(author: 'oscar wild', text: 'be youself'),
-    Quote(author: 'mahmud', text: 'I will win'),
-    Quote(author: 'kazi', text: 'chiro unnoto momo shir')
-  ];
-
-  Widget quoteTemplate(quote) {
-    return QuoteCard(quote: quote);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.green[200],
-        appBar: AppBar(
-          title: const Text('Awesome quote'),
-          centerTitle: true,
-          backgroundColor: Colors.green[800],
-        ),
-        body: Column(
-          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
-        ));
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-  Quote quote;
-
-  //constructor
-  Quote({this.quote});
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Center(
-              child: Text(
-                quote.text,
-                style: TextStyle(
-                  color: Colors.green[600],
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            Center(
-              child: Text(
-                quote.author,
-                style: TextStyle(
-                  color: Colors.green[800],
-                  fontSize: 14,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
